35834164776ca061d539ebb6c33606675cd027be
HWH88340 <hwh88340@gmail.com>
2021-12-21 18:25:44 +0800

    CBV

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,25 +3,43 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    # ORM Implement SQL with Python
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', locals())
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', locals())
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', locals())
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def index(request):
+#     # ORM Implement SQL with Python
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     return render(request, 'polls/index.html', locals())
+#
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', locals())
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', locals())
 
 
 def vote(request, question_id):
@@ -29,7 +47,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html',{
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': 'Incorrect choice'
         })
